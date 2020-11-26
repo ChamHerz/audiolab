6429025989c86758cd5b2e409854ec9224071b83
chamherz <adrielchambers@gmail.com>
2020-11-26 15:24:11 -0300

    rc-dock bien metido

---
+++ src/ui/components/ExplorerTab/ExplorerTab.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ExplorerTab(props) {
+  return (
+    <div>
+      <h1>Soy ExplorerTab</h1>
+    </div>
+  );
+}
--- src/ui/layouts/HomeLayout/HomeLayout.js
+++ src/ui/layouts/HomeLayout/HomeLayout.js
@@ -1,11 +1,129 @@
 import React from "react";
+import { DockLayout, DockContextType } from "rc-dock";
+import ExplorerTab from "../../components/ExplorerTab";
 
 import "./HomeLayout.scss";
 
+let name = window.location.pathname.split("/").pop();
+name = name.substr(0, name.length - 5);
+
+export const jsxTab = {
+  id: "jsxTab",
+  title: "jsx",
+  closable: true,
+  content: <iframe src={`./${name}.jsx.html`} />,
+};
+
+export const htmlTab = {
+  id: "htmlTab",
+  title: "html",
+  closable: true,
+  content: <iframe src={`./${name}.html.html`} />,
+};
+
+let tab = {
+  content: <div>Tab Content</div>,
+  closable: true,
+};
+
+let layout = {
+  dockbox: {
+    mode: "horizontal",
+    children: [
+      {
+        mode: "vertical",
+        size: 200,
+        children: [
+          {
+            tabs: [
+              { ...tab, id: "t1", title: "Tab 1" },
+              { ...tab, id: "t2", title: "Tab 2" },
+            ],
+          },
+          {
+            tabs: [
+              {
+                ...tab,
+                id: "explorerTab",
+                title: "Project:",
+                closable: false,
+                content: <ExplorerTab />,
+                minWidth: 400,
+                minHeight: 300,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        size: 1000,
+        tabs: [
+          {
+            ...tab,
+            id: "t5",
+            title: "basic demo",
+            content: (
+              <div>
+                This panel won't be removed from layout even when last Tab is
+                closed
+              </div>
+            ),
+          },
+          jsxTab,
+          htmlTab,
+        ],
+        panelLock: { panelStyle: "main" },
+      },
+      {
+        size: 200,
+        tabs: [{ ...tab, id: "t8", title: "Tab 8" }],
+      },
+    ],
+  },
+  floatbox: {
+    mode: "float",
+    children: [
+      {
+        tabs: [
+          { ...tab, id: "t9", title: "Tab 9", content: <div>Float</div> },
+          { ...tab, id: "t10", title: "Tab 10" },
+        ],
+        x: 300,
+        y: 150,
+        w: 400,
+        h: 300,
+      },
+    ],
+  },
+};
+if (window.innerWidth < 600) {
+  // remove a column for mobile
+  layout.dockbox.children.pop();
+}
+
+let count = 0;
+
 export default function HomeLayout(props) {
+  /*const onDragNewTab = (e) => {
+    let content = `New Tab ${count++}`;
+    DragStore.dragStart(
+      DockContextType,
+      {
+        tab: {
+          id: content,
+          content: <div style-rc-dock={{ padding: 20 }}>{content}</div>,
+          title: content,
+          closable: true,
+        },
+      },
+      e.nativeEvent
+    );
+  };*/
+
   return (
-    <div>
-      <h1>Soy HomeLayout</h1>
-    </div>
+    <DockLayout
+      defaultLayout={layout}
+      style={{ position: "absolute", left: 10, top: 10, right: 10, bottom: 10 }}
+    />
   );
 }
