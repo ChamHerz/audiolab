5480d0d4eda0c44ff675290c186151815b204927
chamherz <adrielchambers@gmail.com>
2021-01-24 17:55:22 -0300

    dropdown funcional

--- src/ui/components/TopBar/TopBar.js
+++ src/ui/components/TopBar/TopBar.js
@@ -1,28 +1,38 @@
 import React from "react";
-import {Grid, Icon} from "semantic-ui-react";
+import { Grid, Icon } from "semantic-ui-react";
+import { Menu, Dropdown } from "semantic-ui-react";
 
 import "./TopBar.scss";
 
 export default function TopBar(props) {
+  const onSetting = () => {
+    console.log("click en setting");
+  };
 
-    const onSetting = () => {
-        console.log("click en setting");
-    }
+  const options = [
+    { key: 1, text: "This is a super long item", value: 1 },
+    { key: 2, text: "Dropdown direction can help", value: 2 },
+    { key: 3, text: "Items are kept within view", value: 3 },
+  ];
 
-    return (
-        <Grid className="top-bar">
-            <Grid.Row>
-                <Grid.Column width={14}>
-                    <div>Soy TopBar</div>
-                </Grid.Column>
-                <Grid.Column width={2} align="right">
-                    <Icon className='setting-icon'
-                          name='setting'
-                          size='big'
-                          onClick={() => onSetting()}
-                    />
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
-    )
+  return (
+    <Menu>
+      <Dropdown
+        item
+        simple
+        text="Left menu"
+        direction="right"
+        options={options}
+      />
+      <Menu.Menu position="right">
+        <Dropdown item simple icon="setting" direction="right">
+          <Dropdown.Menu>
+            <Dropdown.Item text="Configuración" onClick={() => onSetting()} />
+            <Dropdown.Divider />
+            <Dropdown.Item text="Soporte" />
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu.Menu>
+    </Menu>
+  );
 }
