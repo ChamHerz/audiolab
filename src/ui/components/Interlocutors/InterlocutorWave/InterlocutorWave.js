7bfb6ac7bf94e8e9b93b33ba16a9aedafc9aadb9
chamherz <adrielchambers@gmail.com>
2021-06-02 17:11:51 -0300

    interlocutor Modal

--- src/ui/components/Interlocutors/InterlocutorWave/InterlocutorWave.js
+++ src/ui/components/Interlocutors/InterlocutorWave/InterlocutorWave.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 
 import "./InterlocutorWave.scss";
 import { Button, Grid, Icon } from "semantic-ui-react";
+import InterlocutorListModal from "../../../modals/InterlocutorListModal/InterlocutorListModal";
 
 export default function InterlocutorWave(props) {
+  const [openInterlocutorListModal, setOpenInterlocutorListModal] = useState(
+    false
+  );
+
   const settings = {
     dots: false,
     infinite: false,
@@ -15,27 +20,41 @@ export default function InterlocutorWave(props) {
     className: "interlocutor-wave__list",
   };
 
+  const openModalInterlocutor = () => {
+    setOpenInterlocutorListModal(true);
+    console.log("open modal");
+  };
+
   return (
-    <Grid className="interlocutor-wave">
-      <Grid.Column width={13} className="left">
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-        </Slider>
-      </Grid.Column>
-      <Grid.Column width={3} className="right">
-        <Button className="button-radius">
-          <Icon name="add" />
-          Interlocutor
-        </Button>
-      </Grid.Column>
-    </Grid>
+    <>
+      <Grid className="interlocutor-wave">
+        <Grid.Column width={13} className="left">
+          <Slider {...settings}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+          </Slider>
+        </Grid.Column>
+        <Grid.Column width={3} className="right">
+          <Button
+            className="button-radius"
+            onClick={() => openModalInterlocutor()}
+          >
+            <Icon name="add" />
+            Interlocutor
+          </Button>
+        </Grid.Column>
+      </Grid>
+      <InterlocutorListModal
+        setOpenInterlocutorListModal={setOpenInterlocutorListModal}
+        openInterlocutorListModal={openInterlocutorListModal}
+      />
+    </>
   );
 }
--- src/ui/modals/InterlocutorListModal/InterlocutorListModal.js
+++ src/ui/modals/InterlocutorListModal/InterlocutorListModal.js
@@ -1,11 +1,33 @@
 import React from "react";
 
 import "./InterlocutorListModal.scss";
+import { Button, Modal } from "semantic-ui-react";
 
 export default function InterlocutorListModal(props) {
+  const { openInterlocutorListModal, setOpenInterlocutorListModal } = props;
+
+  const onSubmit = () => {
+    console.log("selected");
+  };
+
   return (
-    <div className="interlocutor-list-modal">
-      <h1>Soy InterlocutorListModal</h1>
-    </div>
+    <Modal
+      className="basic-modal"
+      onClose={() => setOpenInterlocutorListModal(false)}
+      onOpen={() => setOpenInterlocutorListModal(true)}
+      open={openInterlocutorListModal}
+    >
+      <Modal.Header>Seleccionar un Interlocutor</Modal.Header>
+      <Modal.Content>Lista de interlocuotores</Modal.Content>
+      <Modal.Actions>
+        <Button
+          content="Seleccionar Interlocutor"
+          labelPosition="right"
+          icon="checkmark"
+          onClick={() => onSubmit()}
+          positive
+        />
+      </Modal.Actions>
+    </Modal>
   );
 }
